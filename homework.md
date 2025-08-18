5f8fafc0da3cba2b274405a6b21e09c1da1aec59
Aditya <adityachauhan0108@gmail.com>
2025-08-18 17:34:06 +0530

    exploring get,post,delete

--- homework.md
+++ homework.md
@@ -14,3 +14,8 @@
 -.gitignore
 - create a remote repo on github 
 - push all code to remote origin
+-Play with routes and route extensions ex. /hello , / , hello/2 ,/xyz
+-Order of the routes matter a lot
+
+-Install Postman app and  make a workspace/collection > test API call
+-Write logic to handle GET, POST, PATCH, DELETE API Calls and test them on POSTMAN
--- src/app.js
+++ src/app.js
@@ -1,18 +1,42 @@
 const express = require('express');
 const app = express(); //create an instance of express
 
+app.use("/user", (req,res) => {
+    res.send("HAHAHAHAHA")
+})
+
+//This will only handle GET call to /user
+app.get("/user", (req, res) => {
+    res.send({firstName : "Aditya", lastName : "Chauhan"})
+})
+
+app.post("/user",(req,res) => {
+    //saving data to DB
+    res.send("Data successfully saved to the Database")
+})
+
+app.delete("/user",(req,res) => {
+    res.send("Deleted Successfully")
+})
+
+//This will match all the HTTP method API Call to /hello
 app.use("/hello",(req,res) => { //this function is called request handler
     res.send("Hello Hello!"); 
 })
 
-app.use("/",(req,res) => { //this function is called request handler
-    res.send("Hello /!"); 
+app.use("/hello/2",(req,res) => { //this function is called request handler
+    res.send("Hello abc!"); 
 })
 
 app.use("/test",(req,res) => { //this function is called request handler
     res.send("Hello from the server!"); 
 })
 
+app.use("/",(req,res) => { //this function is called request handler
+    res.send("Hello /!"); 
+})
+
+
 app.listen(7777 , () => {
-    console.log("Server is successfully listening on port 3000....");
+    console.log("Server is successfully listening on port 7777....");
 }); //listen incoming requests on port 3000
